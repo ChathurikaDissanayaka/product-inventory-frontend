6174a8d1cd50f8b2c20173b51ca9117d49c0fefb
Sachini <sachichathudissanayaka@gmail.com>
2024-11-04 21:41:54 +0530

    Add changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,51 @@
+import { Container, VStack, Text, Grid } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <Container maxW={"2xl"} py={12}>
+      <VStack spacing={8}>
+        <Text
+          bgGradient={"to-r"}
+          gradientFrom={"#7928CA"}
+          gradientTo={"#FF0080"}
+          bgClip={"text"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          fontSize={30}
+          fontWeight={"bold"}
+        >
+          Welcome to the Product Store!
+        </Text>
 
-export default HomePage
+        <Grid
+          columns={{ base: 1, md: 2, lg: 3 }}
+          spacing={10}
+          w={"full"}
+        ></Grid>
+
+        <Text
+          fontSize={"xl"}
+          textAlign={"center"}
+          fontWeight={"bold"}
+          color={"gray.500"}
+          my={2}
+        >
+          No products found{" "}
+          <Link to={"/create"}>
+            <Text
+              as={"span"}
+              color={"pink.400"}
+              textDecoration={"underline"}
+              _hover={{ color: "pink.300" }}
+            >
+              Create a product
+            </Text>
+          </Link>
+        </Text>
+      </VStack>
+    </Container>
+  );
+};
+
+export default HomePage;

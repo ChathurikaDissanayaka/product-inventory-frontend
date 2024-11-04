98b7ff9827ae855dd2d7ee2698a3a54c95ad8660
Sachini <sachichathudissanayaka@gmail.com>
2024-11-04 18:21:38 +0530

    Add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { Box, ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import  CreatePage  from "./CreatePage";
+import CreatePage from "./CreatePage";
 import Navbar from "./components/Navbar";
+import { ColorModeProvider} from "./components/ui/color-mode";
 
 function App() {
   return (
     <ChakraProvider value={defaultSystem}>
-      <Box minH={"100vh"}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/create" element={<CreatePage />} />
-        </Routes>
-      </Box>
-      
+      <ColorModeProvider>
+          <Box minH={"100vh"}>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/create" element={<CreatePage />} />
+            </Routes>
+          </Box>
+      </ColorModeProvider>
     </ChakraProvider>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,46 @@
-import { Container, Flex } from "@chakra-ui/react"
+import { Container, Flex, HStack, Text, Button } from "@chakra-ui/react";
+import { BsPlusSquare } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { ColorModeButton } from "../components/ui/color-mode";
 
 const Navbar = () => {
   return (
     <Container maxW={"1140px"} px={4}>
-        <Flex>
-            
-        </Flex>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          bgGradient={"to-r"}
+          gradientFrom={"#7928CA"}
+          gradientTo={"#FF0080"}
+          bgClip={"text"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          fontSize={{
+            base: "22",
+            sm: "28",
+          }}
+          fontWeight={"bold"}
+        >
+          <Link to={"/"}>Prodcut Store 🛒</Link>
+        </Text>
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button size="sm" variant="subtle">
+              <BsPlusSquare fontSize={20} />
+            </Button>
+          </Link>
+          <ColorModeButton variant="subtle" />
+        </HStack>
+      </Flex>
     </Container>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
